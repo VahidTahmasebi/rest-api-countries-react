8ed6a31c58fba3a106c21f2fb72ac79c5da48029
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-02-25 18:50:21 +0330

    Add error component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Countries from "./components/Countries";
+import Error from "./components/Error";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className='App'>
         <Routes>
           <Route path='/' element={<Countries />}></Route>
+          <Route path='*' element={<Error />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
